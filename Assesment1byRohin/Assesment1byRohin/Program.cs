5ea991d832571cb68f5f6be01c67e1adf5d1143a
RohinJVU <112471097+RohinJVU@users.noreply.github.com>
2022-08-31 19:44:25 +1000

    test
    I have made necessary changes to make sure the page is organised.

---
+++ Assesment1byRohin/Assesment1byRohin/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+< !DOCTYPE html >
+< html >
+
+< body style = "background-color:powderblue;" >
+
+< center >
+< h1 style = "color:black;" > Fantasy Premier League Scout Picks</h1>
+
+
+<p> This website informs you on who should you pick for the upcoming premier leaague gameweek 5</p>
+
+<center> 
+
+<h2 style="color:black;"> Below are the 3 players that you should pick for Gameweek 5</h2>
+
+<p> Each player listed below have favourable fixtures in the upcoming gameweek</p>
+
+<ul>
+  <li style="color:red;">Mo Salah vs. Everton </li>
+  <li style="color:blue;">Raheem Sterling vs. West Ham</li>
+  <li style="color:maroon;">Ivan toney vs. Leeds United</li>
+</ul> 
+
+    <img src="salah.jpg" alt="Salah" width="275" height="183">
+
+    <p> Mo Salah will be against Liverpools merseyside rivals Everton. After a difficult start to the season
+    for Liverpool, we feel as though he will hit the mark this coming week with his high number in goal
+    contributions this season </p>
+
+
+
+<a href="https://fantasy.premierleague.com/the-scout">Click here to find out more!</a>
+
+</body>
+</html>
+}
+}
